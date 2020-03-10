bcf072dc1dbb79fcbaca72792f7f49f3488ec667
dany_arie <dany_arie@yahoo.com>
2020-03-11 04:10:33 +0700

    add routes handling for list tags

--- main.go
+++ main.go
@@ -5,22 +5,26 @@ import (
 	// "github.com/gin-contrib/static"
 	"github.com/gin-gonic/gin"
 	"bareksa-news/modules/news/api"
+	TagsAPI "bareksa-news/modules/tags/api"
 )
 
 func main() {
 	r := gin.Default()
 
 	NewsController := api.NewsController{}
+	TagsController := TagsAPI.TagsController{}
 	//Setup CORS
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowOrigins = []string{"*"}
 	r.Use(cors.New(corsConfig))
 	r.StaticFile("/app.log", "./tmp/app.log")
-
+	//News
 	r.GET("/", responseToUser)
 	r.GET("/news", NewsController.List)
 	r.GET("/news/:id", NewsController.GetOne)
 	r.POST("/news/update", NewsController.UpdateOne)
+	//Tags
+	r.GET("/tags", TagsController.List)
 
 	r.Run()
 }
