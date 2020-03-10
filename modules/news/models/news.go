082c59aca5109b8a21901bfbd312abce31e38f75
dany_arie <dany_arie@yahoo.com>
2020-03-11 04:11:10 +0700

    update news model

--- modules/news/models/news.go
+++ modules/news/models/news.go
@@ -1,9 +1,12 @@
 package models
 
+import TagsModel "bareksa-news/modules/tags/models"
+
 // News struct
 type News struct {
-	ID           int      `json:"id"`
-	Title        string   `json:"title"`
-	Description  string   `json:"description"`
-	Status       string    `json:"status"`
+	ID           int      			`json:"id"`
+	Title        string   			`json:"title"`
+	Description  string   			`json:"description"`
+	Status       string    			`json:"status"`
+	Tags         []TagsModel.Tags 	`json:"tag"`
 }
