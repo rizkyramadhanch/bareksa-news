6c25c2b484cb6d7bc1857616db795d0a9d3ce60b
dany_arie <dany_arie@yahoo.com>
2020-03-11 03:10:30 +0700

    add routes handling for news service

--- main.go
+++ main.go
@@ -3,16 +3,22 @@ package main
 import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"bareksa-news/modules/news/api"
 )
 
 func main() {
 	r := gin.Default()
+
+	NewsController := api.NewsController{}
 	//Setup CORS
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowOrigins = []string{"*"}
 	r.Use(cors.New(corsConfig))
 
 	r.GET("/", responseToUser)
+	r.GET("/news", NewsController.List)
+	r.GET("/news/:id", NewsController.GetOne)
+	r.POST("/news/update", NewsController.UpdateOne)
 
 	r.Run()
 }
@@ -23,3 +29,6 @@ func responseToUser(c *gin.Context) {
 		"Status" : "OK",
 	})
 }
+
+
+
