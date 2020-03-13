37c6c1a194d8290c9f4450863da5727be1f413e6
dany_arie <dany_arie@yahoo.com>
2020-03-13 15:10:19 +0700

    add switch mode env on database

--- database/connection.go
+++ database/connection.go
@@ -7,27 +7,62 @@ import (
 	"fmt"
 	"bareksa-news/config"
 	"bareksa-news/library/logger"
+	"github.com/gin-gonic/gin"
+	// "strconv"
 )
 
 // DB for querying and transaction
 var DB *sql.DB
 
+var psqlInfo string
+
 func init() {
-	psqlInfo := fmt.Sprintf("host=%s port=%s user=%s "+
+	/*
+		Uncommand code below to running in production mode
+	*/
+	gin.SetMode(gin.ReleaseMode)
+
+	if gin.Mode() == "debug" {
+		logger.Log.Println("entering debug mode")
+		psqlInfo = fmt.Sprintf("host=%s port=%d user=%s "+
 		"password=%s dbname=%s sslmode=disable",
-		os.Getenv("host"), os.Getenv("port"), os.Getenv("user"), os.Getenv("password"), os.Getenv("name"))
+		config.Config.DB.Host, config.Config.DB.Port, config.Config.DB.User, config.Config.DB.Password, config.Config.DB.Name)
 
-	var err error
-	fmt.Println(config.Config.DB.Name)
-	DB, err = sql.Open("postgres", psqlInfo)
+		var err error
+		fmt.Println(config.Config.DB.Name)
+		DB, err = sql.Open("postgres", psqlInfo)
 
-	if err != nil {
-		panic(err)
+		if err != nil {
+			panic(err)
+		}
+
+		if err = DB.Ping(); err != nil {
+			panic(err)
+		}
+
+		logger.Log.Println("[APP] Database successfully connected!")
+	} else if gin.Mode() == "release" {
+		logger.Log.Println("entering release mode")
+
+		fmt.Println(os.Getenv("host"))
+		
+		psqlInfo = fmt.Sprintf("host=%s port=%d user=%s "+
+		"password=%s dbname=%s sslmode=disable",
+		os.Getenv("host"), 5432, os.Getenv("user"), os.Getenv("password"), os.Getenv("name"))
+
+		var err error
+		DB, err = sql.Open("postgres", psqlInfo)
+
+		if err != nil {
+			panic(err)
+		}
+
+		if err = DB.Ping(); err != nil {
+			panic(err)
+		}
+
+		logger.Log.Println("[APP] Database successfully connected!")
+	}	 else {
+		os.Exit(500)
 	}
-
-	if err = DB.Ping(); err != nil {
-		panic(err)
-	}
-
-	logger.Log.Println("[APP] Database successfully connected!")
 }
